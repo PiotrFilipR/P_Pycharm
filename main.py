e48f9d2c06a6c48c7b20c798005bf016fe651468
Filip <rytel.filip@gmail.com>
2022-12-21 20:38:21 +0100

    Wprowadzono zaokraglenie liczb

--- main.py
+++ main.py
@@ -39,32 +39,31 @@ Sty1 = (1+((I_Sty1 + opr_kredyt)/1200)) * pocz_kredyt - stala_rata
 print("Styczeń 2023:", Sty1)
 
 Lut1 = (1+((I_Lut1 + opr_kredyt)/1200)) * Sty1 - stala_rata
-print("Luty 2023:", Lut1)
+print("Luty 2023:", round(Lut1, 2))
 
 Mar1 = (1+((I_Mar1 + opr_kredyt)/1200)) * Lut1 - stala_rata
-print("Marzec 2023:", Mar1)
+print("Marzec 2023:", round(Mar1, 2))
 
 Kwi1 = (1+((I_Kwi1 + opr_kredyt)/1200)) * Mar1 - stala_rata
-print("Kwiecień 2023:", Kwi1)
+print("Kwiecień 2023:", round(Kwi1, 2))
 
 Maj1 = (1+((I_Maj1 + opr_kredyt)/1200)) * Kwi1 - stala_rata
-print("Maj 2023:", Maj1)
+print("Maj 2023:", round(Maj1, 2))
 
 Cze1 = (1+((I_Cze1 + opr_kredyt)/1200)) * Maj1 - stala_rata
-print("Czerwiec 2023:", Cze1)
+print("Czerwiec 2023:", round(Cze1, 2))
 
 Sie1 = (1+((I_Sie1 + opr_kredyt)/1200)) * Cze1 - stala_rata
-print("Sierpień 2023:", Sie1)
+print("Sierpień 2023:", round(Sie1, 2))
 
 Wrz1 = (1+((I_Wrz1 + opr_kredyt)/1200)) * Sie1 - stala_rata
-print("Wrzesień 2023:", Wrz1)
+print("Wrzesień 2023:", round(Wrz1, 2))
 
 Paz1 = (1+((I_Paz1 + opr_kredyt)/1200)) * Wrz1 - stala_rata
-print("Październik 2023:", Paz1)
+print("Październik 2023:", round(Paz1, 2))
 
 Lis1 = (1+((I_Lis1 + opr_kredyt)/1200)) * Paz1 - stala_rata
-print("Listopad 2023:", Lis1)
+print("Listopad 2023:", round(Lis1, 2))
 
 Gru1 = (1+((I_Gru1 + opr_kredyt)/1200)) * Lis1 - stala_rata
-print("Grudzień 2023:", Gru1)
-
+print("Grudzień 2023:", round(Gru1, 2))
