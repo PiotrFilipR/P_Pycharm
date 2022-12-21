42d0ed956776750bff585ac8cf4cdd5354921c51
Filip <rytel.filip@gmail.com>
2022-12-21 19:56:42 +0100

    Określono początkowe wartości - input

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 
+# przywitanie
+
+print("Witaj w programie, który wyliczy wysokość Twoich rat przez najbliższe 2 lata")
+
+enter = input('Naciśnij "enter", aby przejść dalej ')
+
+# wyskakuj z informacji
+
+pocz_kredyt = input("1. Podaj początkową wysokość kredytu [PLN]: ")
+pocz_kredyt = float(pocz_kredyt)
+
+opr_kredyt = input("2. Podaj oprocentowanie kredytu [%]: ")
+opr_kredyt = float(opr_kredyt)
+
+stala_rata = input("3. Podaj stałą wartość raty [PLN]: ")
+stala_rata = float(stala_rata)
+
+
