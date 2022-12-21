c5df72c8fd1ec46ada21bde319f52e68aa9618c6
Filip <rytel.filip@gmail.com>
2022-12-21 20:13:01 +0100

    Wprowadzono wartość inflacji

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
 # przywitanie
 
-print("Witaj w programie, który wyliczy wysokość Twoich rat przez najbliższe 2 lata")
+print("Witaj w programie, który wyliczy wysokość Twojego zadłużenia w poszczególnych miesiąc przez najbliższe 2 lata")
 
-enter = input('Naciśnij "enter", aby przejść dalej ')
+enter = input('Naciśnij "enter", aby rozpocząć ')
 
 # wyskakuj z informacji
 
@@ -15,5 +15,22 @@ opr_kredyt = float(opr_kredyt)
 
 stala_rata = input("3. Podaj stałą wartość raty [PLN]: ")
 stala_rata = float(stala_rata)
+
+# Wprowadzone wartości inflacji
+
+I_Sty1 = 1,59
+I_Lut1 = -0,45
+I_Mar1 = 2,32
+I_Kwi1 = 1,26
+I_Maj1 = 1,78
+I_Cze1 = 2,33
+I_Lip1 = 1,50
+I_Sie1 = 1,78
+I_Wrz1 = 2,33
+I_Paz1 = 0,62
+I_Lis1 = 2,35
+I_Gru1 = 0,34
+
+# Obliczenia dla poszczególnych miesięcy
 
 
