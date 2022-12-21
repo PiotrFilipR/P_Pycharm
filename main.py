5491f7e699920ba3ddb94cc5990ca480ab8b7f85
Filip <rytel.filip@gmail.com>
2022-12-21 20:34:08 +0100

    Wprowadzono pierwszy rok - bez odpowiedniego formatu liczb

--- main.py
+++ main.py
@@ -18,19 +18,53 @@ stala_rata = float(stala_rata)
 
 # Wprowadzone wartości inflacji
 
-I_Sty1 = 1,59
-I_Lut1 = -0,45
-I_Mar1 = 2,32
-I_Kwi1 = 1,26
-I_Maj1 = 1,78
-I_Cze1 = 2,33
-I_Lip1 = 1,50
-I_Sie1 = 1,78
-I_Wrz1 = 2,33
-I_Paz1 = 0,62
-I_Lis1 = 2,35
-I_Gru1 = 0,34
+I_Sty1 = 1.59
+I_Lut1 = -0.45
+I_Mar1 = 2.32
+I_Kwi1 = 1.26
+I_Maj1 = 1.78
+I_Cze1 = 2.33
+I_Lip1 = 1.50
+I_Sie1 = 1.78
+I_Wrz1 = 2.33
+I_Paz1 = 0.62
+I_Lis1 = 2.35
+I_Gru1 = 0.34
 
 # Obliczenia dla poszczególnych miesięcy
 
+print("Twoje zadłużenie w poszczególnych miesiącach:")
+
+Sty1 = (1+((I_Sty1 + opr_kredyt)/1200)) * pocz_kredyt - stala_rata
+print("Styczeń 2023:", Sty1)
+
+Lut1 = (1+((I_Lut1 + opr_kredyt)/1200)) * Sty1 - stala_rata
+print("Luty 2023:", Lut1)
+
+Mar1 = (1+((I_Mar1 + opr_kredyt)/1200)) * Lut1 - stala_rata
+print("Marzec 2023:", Mar1)
+
+Kwi1 = (1+((I_Kwi1 + opr_kredyt)/1200)) * Mar1 - stala_rata
+print("Kwiecień 2023:", Kwi1)
+
+Maj1 = (1+((I_Maj1 + opr_kredyt)/1200)) * Kwi1 - stala_rata
+print("Maj 2023:", Maj1)
+
+Cze1 = (1+((I_Cze1 + opr_kredyt)/1200)) * Maj1 - stala_rata
+print("Czerwiec 2023:", Cze1)
+
+Sie1 = (1+((I_Sie1 + opr_kredyt)/1200)) * Cze1 - stala_rata
+print("Sierpień 2023:", Sie1)
+
+Wrz1 = (1+((I_Wrz1 + opr_kredyt)/1200)) * Sie1 - stala_rata
+print("Wrzesień 2023:", Wrz1)
+
+Paz1 = (1+((I_Paz1 + opr_kredyt)/1200)) * Wrz1 - stala_rata
+print("Październik 2023:", Paz1)
+
+Lis1 = (1+((I_Lis1 + opr_kredyt)/1200)) * Paz1 - stala_rata
+print("Listopad 2023:", Lis1)
+
+Gru1 = (1+((I_Gru1 + opr_kredyt)/1200)) * Lis1 - stala_rata
+print("Grudzień 2023:", Gru1)
 
