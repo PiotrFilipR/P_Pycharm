2ca75f8e795f5a08464e4ffd8fd513827f5951d4
Filip <rytel.filip@gmail.com>
2022-12-22 16:31:19 +0100

    Wszystkie miesiące + pożegnanie - gotowy program

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 # przywitanie
 
-print("Witaj w programie, który wyliczy wysokość Twojego zadłużenia w poszczególnych miesiąc przez najbliższe 2 lata")
+print("Witaj w programie, który wyliczy wysokość Twojego zadłużenia w poszczególnych miesiącach przez najbliższe 2 lata")
 
 enter = input('Naciśnij "enter", aby rozpocząć ')
 
@@ -30,13 +30,26 @@ I_Wrz1 = 2.33
 I_Paz1 = 0.62
 I_Lis1 = 2.35
 I_Gru1 = 0.34
+I_Sty2 = 1.58
+I_Lut2 = -0.29
+I_Mar2 = 2.49
+I_Kwi2 = 0.27
+I_Maj2 = 1.42
+I_Cze2 = 1.05
+I_Lip2 = 1.48
+I_Sie2 = 1.58
+I_Wrz2 = -0.08
+I_Paz2 = 1.17
+I_Lis2 = -0.4
+I_Gru2 = 1.50
+
 
 # Obliczenia dla poszczególnych miesięcy
 
-print("Twoje zadłużenie w poszczególnych miesiącach:")
+print("Twoje zadłużenie w poszczególnych miesiącach [PLN]:")
 
 Sty1 = (1+((I_Sty1 + opr_kredyt)/1200)) * pocz_kredyt - stala_rata
-print("Styczeń 2023:", Sty1)
+print("Styczeń 2023:", round(Sty1, 2))
 
 Lut1 = (1+((I_Lut1 + opr_kredyt)/1200)) * Sty1 - stala_rata
 print("Luty 2023:", round(Lut1, 2))
@@ -53,7 +66,10 @@ print("Maj 2023:", round(Maj1, 2))
 Cze1 = (1+((I_Cze1 + opr_kredyt)/1200)) * Maj1 - stala_rata
 print("Czerwiec 2023:", round(Cze1, 2))
 
-Sie1 = (1+((I_Sie1 + opr_kredyt)/1200)) * Cze1 - stala_rata
+Lip1 = (1+((I_Lip1 + opr_kredyt)/1200)) * Cze1 - stala_rata
+print("Lipiec 2023:", round(Lip1, 2))
+
+Sie1 = (1+((I_Sie1 + opr_kredyt)/1200)) * Lip1 - stala_rata
 print("Sierpień 2023:", round(Sie1, 2))
 
 Wrz1 = (1+((I_Wrz1 + opr_kredyt)/1200)) * Sie1 - stala_rata
@@ -67,3 +83,42 @@ print("Listopad 2023:", round(Lis1, 2))
 
 Gru1 = (1+((I_Gru1 + opr_kredyt)/1200)) * Lis1 - stala_rata
 print("Grudzień 2023:", round(Gru1, 2))
+
+Sty2 = (1+((I_Sty2 + opr_kredyt)/1200)) * Gru1 - stala_rata
+print("Styczeń 2024:", round(Sty2, 2))
+
+Lut2 = (1+((I_Lut2 + opr_kredyt)/1200)) * Sty2 - stala_rata
+print("Luty 2024:", round(Lut2, 2))
+
+Mar2 = (1+((I_Mar2 + opr_kredyt)/1200)) * Lut2 - stala_rata
+print("Marzec 2024:", round(Mar2, 2))
+
+Kwi2 = (1+((I_Kwi2 + opr_kredyt)/1200)) * Mar2 - stala_rata
+print("Kwiecień 2024", round(Kwi2, 2))
+
+Maj2 = (1+((I_Maj2 + opr_kredyt)/1200)) * Kwi2 - stala_rata
+print("Maj 2024:", round(Maj2, 2))
+
+Cze2 = (1+((I_Cze2 + opr_kredyt)/1200)) * Maj2 - stala_rata
+print("Czerwiec 2024:", round(Cze2, 2))
+
+Lip2 = (1+((I_Lip2 + opr_kredyt)/1200)) * Cze2 - stala_rata
+print("Lipiec 2024:", round(Lip2, 2))
+
+Sie2 = (1+((I_Sie2 + opr_kredyt)/1200)) * Lip2 - stala_rata
+print("Sierpień 2024:", round(Sie2, 2))
+
+Wrz2 = (1+((I_Wrz2 + opr_kredyt)/1200)) * Sie2 - stala_rata
+print("Wrzesień 2024:", round(Wrz2, 2))
+
+Paz2 = (1+((I_Paz2 + opr_kredyt)/1200)) * Wrz2 - stala_rata
+print("Październik 2024:", round(Paz2, 2))
+
+Lis2 = (1+((I_Lis2 + opr_kredyt)/1200)) * Paz2 - stala_rata
+print("Listopad 2024:", round(Lis2, 2))
+
+Gru2 = (1+((I_Gru2 + opr_kredyt)/1200)) * Lis2 - stala_rata
+print("Grudzień 2024:", round(Gru2, 2))
+
+# Pożegnanie
+print("Dzięki za skorzystanie z programu! Do zobaczenia!")
